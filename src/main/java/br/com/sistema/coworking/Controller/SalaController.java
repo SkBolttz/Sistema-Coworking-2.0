efabc1431f3ab86a1c553e7dbecf5552bdc23979
henrique <37087436+SkBolttz@users.noreply.github.com>
2025-06-26 18:32:33 -0300

    Controller Sala documento com Swagger

--- src/main/java/br/com/sistema/coworking/Controller/SalaController.java
+++ src/main/java/br/com/sistema/coworking/Controller/SalaController.java
@@ -9,10 +9,17 @@ import br.com.sistema.coworking.DTO.Sala.AtualizarSalaDTO;
 import br.com.sistema.coworking.Entity.Sala;
 import br.com.sistema.coworking.Exception.Records.Sala.CadastroSalaExecption;
 import br.com.sistema.coworking.Service.SalaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/sala")
+@Tag(name = "Sala", description = "Controller responsável por gerenciar as informações das salas.")
 public class SalaController {
 
     private final SalaService salaService;
@@ -22,6 +29,14 @@ public class SalaController {
     }
 
     @PostMapping("/criar")
+    @Operation(summary = "Criar sala", description = "Cria uma nova sala com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "sala", description = "Informações da sala para criar."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar sala."),
+    })
     public ResponseEntity<String> criarSala(@RequestBody @Valid Sala sala) {
         try {
             salaService.criarSala(sala);
@@ -32,6 +47,14 @@ public class SalaController {
     }
 
     @PutMapping("/atualizar")
+    @Operation(summary = "Atualizar sala", description = "Atualiza uma sala existente com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "atualizarSala", description = "Informações da sala para atualizar."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala atualizada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar sala."),
+    })
     public ResponseEntity<String> atualizarSala(@RequestBody @Valid AtualizarSalaDTO atualizarSala) {
         try {
             salaService.atualizarSala(atualizarSala);
@@ -42,6 +65,14 @@ public class SalaController {
     }
 
     @PutMapping("/ativar")
+    @Operation(summary = "Ativar sala", description = "Ativa uma sala existente com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "atualizarSala", description = "Informações da sala para ativar."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala ativada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao ativar sala."),
+    })
     public ResponseEntity<String> ativarSala(@RequestBody @Valid AtualizarSalaDTO atualizarSala) {
         try {
             salaService.ativarSala(atualizarSala);
@@ -52,6 +83,14 @@ public class SalaController {
     }
 
     @PutMapping("/desativar")
+    @Operation(summary = "Desativar sala", description = "Desativa uma sala existente com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "atualizarSala", description = "Informações da sala para desativar."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sala desativada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao desativar sala."),
+    })
     public ResponseEntity<String> desativarSala(@RequestBody @Valid AtualizarSalaDTO atualizarSala) {
         try {
             salaService.desativarSala(atualizarSala);
@@ -62,6 +101,14 @@ public class SalaController {
     }
 
     @PostMapping("/dados")
+    @Operation(summary = "Obter dados da sala", description = "Obtem os dados da sala com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "atualizarSala", description = "Informações da sala para obter."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados obtidos com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao obter dados."),
+    })
     public ResponseEntity<Sala> obterDadosSala(@RequestBody AtualizarSalaDTO atualizarSala) {
         try {
             Sala sala = salaService.obterDadosSala(atualizarSala);
@@ -72,6 +119,14 @@ public class SalaController {
     }
 
     @GetMapping("/listar-todas")
+    @Operation(summary = "Listar todas as salas", description = "Lista todas as salas com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "pageable", description = "Informações da sala para obter."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salas obtidas com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao obter salas."),
+    })
     public ResponseEntity<Page<Sala>> listarSalas(
             @PageableDefault(size = 10) Pageable pageable) {
         try {
@@ -83,6 +138,14 @@ public class SalaController {
     }
 
     @GetMapping("/listar-disponiveis")
+    @Operation(summary = "Listar salas disponíveis", description = "Lista as salas disponíveis com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "pageable", description = "Informações da sala para obter."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salas obtidas com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao obter salas."),
+    })
     public ResponseEntity<Page<Sala>> listarSalasDisponiveis(
             @PageableDefault(size = 10) Pageable pageable) {
         try {
@@ -94,6 +157,14 @@ public class SalaController {
     }
 
     @GetMapping("/listar-indisponiveis")
+    @Operation(summary = "Listar salas indisponíveis", description = "Lista as salas indisponíveis com base nas informações fornecidas no corpo da requisição.")
+    @Parameters({
+            @Parameter(name = "pageable", description = "Informações da sala para obter."),
+    })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salas obtidas com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao obter salas."),
+    })
     public ResponseEntity<Page<Sala>> listarSalasIndisponiveis(
             @PageableDefault(size = 10) Pageable pageable) {
         try {

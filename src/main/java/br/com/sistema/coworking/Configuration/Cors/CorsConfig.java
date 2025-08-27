be555beeb614e03b757aaa1fda7b5a115f9829f7
henrique <37087436+SkBolttz@users.noreply.github.com>
2025-08-27 20:16:45 -0300

    Ajustando url front

--- src/main/java/br/com/sistema/coworking/Configuration/Cors/CorsConfig.java
+++ src/main/java/br/com/sistema/coworking/Configuration/Cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://nexuscoworking.vercel.app/")
                         .allowedMethods("*") 
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- src/main/java/br/com/sistema/coworking/Security/SecurityConfig.java
+++ src/main/java/br/com/sistema/coworking/Security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("https://nexuscoworking.vercel.app/"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
